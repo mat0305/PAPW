cdb374a141680176473a038c09cd24b3b4eef5aa
mat0305 <ondra.3110@seznam.cz>
2016-10-05 10:20:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="">
+  <head>  
+   
+    <title>PAW</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="7 days">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		
+</head>
+<body>
+
+<?php
+	print "Ahoj Karle tady máš odrážky:";
+	for($i=0; $i<10; $i++) {
+		print "<li>Odrážka č.".$i."</li>";
+	}
+
+?>
+
+</body>
+</html>
