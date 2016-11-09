d04ce3272ae98b2998834a8d7688c60331645ed3
mat0305 <ondra.3110@seznam.cz>
2016-11-09 10:28:42 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="">
   <head>  
-   
+
     <title>PAW</title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
@@ -9,55 +9,226 @@
 	<meta name="robots" content="index, follow">
 	<meta name="revisit-after" content="7 days">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	  
+ 
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+	<script src="https://unpkg.com/leaflet@1.0.1/dist/leaflet.js"></script>
+	<script src="http://gisak.vsb.cz/ruzicka/lib/leaflet/showplace.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+    $("#1").click(function(){
+        $(this).hide();
+    });
+});
+</script>
+
+<script>
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div").toggle();
+    });
+});
+</script>
+
+<script>
+	$(document).ready(function(){
 		
+		setMap('mapid');
+		$("#mapid").hide();
+		
+		$("strong").click(function(){
+			
+			$("#mapid").show();
+			
+			var lat = $(this).attr("lat");
+			var lon = $(this).attr("lon");
+			var zoom = $(this).attr("zoom");
+			var text = $(this).attr("text");
+			
+			showPlace(lat, lon, zoom, text);
+		});
+	});
+	</script>	
+
+   
 </head>
 <body>
- <body bgcolor="blue" text="white" link="green" vlink="olive" alink="#FF0000">
-<form>
-<p>
-Zadej počet:<input name="pocet" value="5"/>
-Zadej slovo:<input name="info" value="Pes"/><br>
-<input type="radio" name="style" value="li" checked> Odrážka<br>
-<input type="radio" name="style" value="p"> Odstavec<br>
-<p><input type="submit"/>
-<p><textarea name="text" cols="70" rows="8">
-Tady má být nějaká věta.
-</textarea>
 
-</p>
+
+
+
+
+<div> 
+<p>odstavec</p>
+<p>další odstavec</p>
+</div>
+
+<p><button>Klikni tady</button></p>
+
+<form>
+	<p>
+	Počet:<input type="number" name="pocet" min="0" max="100" step="1" value="30">
+	Info:<input name="info" value="FFF"/>
+	Birthday (date and time): <input type="datetime-local" name="bdaytime">
+	<input type="radio" name="style" value="li" checked> Odrážka<br>
+	<input type="radio" name="style" value="p"> Odstavec<br>
+	<textarea name="text1" rows="10" cols="50">
+		Ostrava je pěkná díra. Vítejte v Ostravě. V Ostravě je pěkně. Ostravou táhnou šílené opice.
+	</textarea>
+	<input type="submit"/>	
+	</p>
 </form>
 
+<p>
+<strong lat="49.81" lon="18.26" zoom="10" text="Ostrava">
+Ostrava</strong> je tady
+</p>
+
+<div id="mapid" style="width: 600px; height: 400px;"></div>
 
 
 <?php
+function connect ($misto) {
+// Let's pass in a $_GET variable to our example, in this case
+// it's aid for actor_id in our Sakila database. Let's make it
+// default to 1, and cast it to an integer as to avoid SQL injection
+// and/or related security problems. Handling all of this goes beyond
+// the scope of this simple example. Example:
+//   http://example.org/script.php?aid=42
+if (isset($_GET['aid']) && is_numeric($_GET['aid'])) {
+    $aid = (int) $_GET['aid'];
+} else {
+    $aid = 1;
+}
 
-	print "<h1>Ahoj</h1>";
-	print "<p>".$_REQUEST["text"]. "</p>";
-	$pocet_znaku = strlen($_REQUEST["text"]);
-	print $pocet_znaku ;
-	
-	
-$pieces = explode(" ", $_REQUEST["text"]);
-print "<p>".$pieces[0]."</p>";
-print "<p>".$pieces[1]."</p>";
-print "<p>".$pieces[2]."</p>";
- 
+// Connecting to and selecting a MySQL database named sakila
+// Hostname: 127.0.0.1, username: your_user, password: your_pass, db: sakila
+$mysqli = new mysqli('127.0.0.1', 'root', 'root', 'mat0305');
 
+// Oh no! A connect_errno exists so the connection attempt failed!
+if ($mysqli->connect_errno) {
+    // The connection failed. What do you want to do? 
+    // You could contact yourself (email?), log the error, show a nice page, etc.
+    // You do not want to reveal sensitive information
+
+    // Let's try this:
+    echo "Sorry, this website is experiencing problems.";
+
+    // Something you should not do on a public site, but this example will show you
+    // anyways, is print out MySQL error related information -- you might log this
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    
+    // You might want to show them something nice, but we will simply exit
+    exit;
+}
+
+// Perform an SQL query
+$sql = "SELECT * FROM mista WHERE nazev LiKE '%".$misto."%'";
+if (!$result = $mysqli->query($sql)) {
+    // Oh no! The query failed. 
+    echo "Sorry, the website is experiencing problems.";
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+
+
+
+// Phew, we made it. We know our MySQL connection and query 
+// succeeded, but do we have a result?
+if ($result->num_rows === 0) {
+    // Oh, no rows! Sometimes that's expected and okay, sometimes
+    // it is not. You decide. In this case, maybe actor_id was too
+    // large? 
+    echo "We could not find a match for ID $aid, sorry about that. Please try again.";
+    exit;
+}
+
+// Now, we know only one result will exist in this example so let's 
+// fetch it into an associated array where the array's keys are the 
+// table's column names
+$actor = $result->fetch_assoc();
+echo "Sometimes I see " . $actor['nazev'] . " " . $actor['lat'] . " on TV.";
+
+// Now, let's fetch five random actors and output their names to a list.
+// We'll add less error handling here as you can do that on your own now
+$sql = "SELECT * FROM mista ORDER BY rand() LIMIT 5";
+if (!$result = $mysqli->query($sql)) {
+    echo "Sorry, the website is experiencing problems.";
+    exit;
+}
+
+// Print our 5 random actors in a list, and link to each actor
+echo "<ul>\n";
+while ($actor = $result->fetch_assoc()) {
+    echo "<li><a href='" . $_SERVER['SCRIPT_FILENAME'] . "?aid=" . $actor['id'] . "'>\n";
+    echo $actor['nazev'] . ' ' . $actor['lat'];
+    echo "</a></li>\n";
+}
+echo "</ul>\n";
+
+// The script will automatically free the result and close the MySQL
+// connection when it exits, but let's just do it anyways
+$result->free();
+$mysqli->close();
+}
+
+
+
+	echo "<h1>Ahoj Karle</h1>";
+	//echo "<p>".$_REQUEST["text1"]."</p>";
+	
+	$pocet_znaku = strlen($_REQUEST["text1"]);
+	//echo "<p>".$pocet_znaku."</p>";
+	
+	$pieces = explode(" ", $_REQUEST["text1"]);
 	/*
-	print $_REQUEST["pocet"];
+	echo "<p>".$pieces[0]."</p>"; // piece1
+	echo "<p>".$pieces[1]."</p>"; // piece2
+	*/
 	
-	 print "Ahoj Karle tady máš odrážky:";
+	/*
+	foreach ($pieces as $value) {
+		echo "<p>".$value."</p>";
+	}
+	*/
 	
-	for($i=0; $i<$_REQUEST["pocet"]; $i++) {
-		 if ($_REQUEST["style"] == "li") {
-		 print "<li>Odrážka č.".$i." ".$_REQUEST["info"]."</li>";
-		} else {
-			print "<p>Odstavec č.".$i." ".$_REQUEST["info"]."</p>";
+	$ostrava = array("Ostrava", "Ostravě", "Ostravou"); 
+	
+	$vyber=0;
+	for ($i=0; $i<count($pieces); $i++) {
+		$word = trim($pieces[$i], ". \t\n");
+		$word = str_replace(".", "", $word);
+		if (in_array($word, $ostrava)) {
+			
+			$vyber++;
+			$pieces[$i] = "<strong id=\"".$vyber."\">".$pieces[$i]."</strong>";
 		}
 	}
-*/
-	
+	$text1_strong_ostrava = implode(" ", $pieces);
+	echo "<p>".$text1_strong_ostrava."</p>";
+	/*
+	echo "<ol>";
+	for ($i=1; $i<=$_REQUEST["pocet"]; $i++) {
+		if ($_REQUEST["style"] == "li") {
+			echo "<li>Odrážka č.".$i." ".$_REQUEST["info"]."</li>";
+		} else {
+			echo "<p>Odstavec č.".$i." ".$_REQUEST["info"]."</p>";
+		}
+	}
+	echo "</ol>";	
+	*/
+	connect("Ostrava");
 ?>
-
 </body>
 </html>
